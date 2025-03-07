5a12d4e552b7e50e70f188c18f3a5a4a496a12ae
Kelly Pointer <kellyjoanpointer@gmail.com>
2025-03-06 23:17:14 -0600

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+const burgers = ["Hamburger", "Cheeseburger"];
+const featuredDrink = ["Strawberry Milkshake"];
+
+
+function addBurger() {
+const newBurger = "Flatburger";
+burgers.push(newBurger)
+}
+addBurger()
+
+if(8<9) {
+    let anothernewBurger = "Maple Bacon Burger"
+    burgers.push(anothernewBurger)
+}
+function changeFeaturedDrink() {featuredDrink[0] = "The Java Shake"}
+
+changeFeaturedDrink()
+console.log(burgers);
+console.log(featuredDrink);
